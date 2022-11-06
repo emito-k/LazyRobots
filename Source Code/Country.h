8fcc45cfb0cb3cf18cef70c4d3632ae0b175899a
emito-k <65490872+emito-k@users.noreply.github.com>
2022-11-06 22:44:36 +0200

    Added isActive method
    Created a new method to get isActive state of the flag, if it returns false and added some comments

--- Source Code/Country.h
+++ Source Code/Country.h
@@ -15,12 +15,34 @@ class Country {
         // @param countryName is the name of the country
         // @returns nothing
         Country(string countryName);
+        /**
+        * Changes the state of the flag from home flag to white flag
+        */
         void surrender();
+        /**
+        * Prints out a list of all the army units it control
+        */
         void printArmies();
+        /**
+        * The function executes the executeCommand method of @param playerCommand 
+        */
         void executeCommand(PlayerCommand *playerCommand);
+        /**
+        * The function sets @param node to be the capitol of the Country object
+        */
         void setCapitol(Node* node);
+        /**
+        * @returns the name of the country object
+        */
         std::string getCountryName();
+        /**
+        * The function adds @armyUnit into the array of armies
+        */
         void addArmy(ArmyUnit* armyUnit);
+        /**
+        * @returns the result of the flag state isActive function
+        */
+        bool isActive();
 
     private:
         Node* capital;
