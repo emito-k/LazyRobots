bc8d37590ad86ea547cad8464689bbebc2b9951d
emito-k <65490872+emito-k@users.noreply.github.com>
2022-11-07 22:13:14 +0200

    Added comments for Doxygen

--- Source Code/ArmyAttackCommand.h
+++ Source Code/ArmyAttackCommand.h
@@ -5,7 +5,13 @@
 
 class ArmyAttackCommand : public PlayerCommand {
 public:
+    /**
+    * Constructor that intializes the PlayerCommand parent function with "Attack Commad"
+    */
     ArmyAttackCommand();
+    /*
+    * This function uses @param country to issue an attack command on enemy units
+    */
     void executeCommand(Country *country) override;
 };
 
