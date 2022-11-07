0d22cd77cc64fb2d46f78a862f4894a29398312d
emito-k <65490872+emito-k@users.noreply.github.com>
2022-11-07 23:09:18 +0200

    Update Terrain.h

--- Source Code/Terrain.h
+++ Source Code/Terrain.h
@@ -6,9 +6,14 @@ class ArmyUnit;
 
 class Terrain {
 public:
+    
+    /**
+    * This sets the #terrainType
+    */
     Terrain(std::string terrainType);
     virtual void addEffects(ArmyUnit* armyUnit) = 0;
     virtual void removeEffects(ArmyUnit* armyUnit) = 0;
+    void getTerrainType() const;
 private:
     std::string _terrainType;
 };
