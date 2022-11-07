b28c541d944c509e9e4db29aa23cee22fd91ae3c
emito-k <65490872+emito-k@users.noreply.github.com>
2022-11-07 23:05:15 +0200

    Add comments

--- Source Code/WarEngine.h
+++ Source Code/WarEngine.h
@@ -8,9 +8,22 @@
 
 class WarEngine {
 public:
+    /**
+    * Instantiates the #graph variable
+    */
     WarEngine();
+    /**
+    * This is the main war loop for the program
+    */
     void warStart();
+    /**
+    * This creates a series of connect nodes to make a graph which is the map
+    */
     void createMap();
+    
+    /**
+    * This function creates the Player objects that wiil be playing the game
+    */
     void createPlayers();
 
 private:
