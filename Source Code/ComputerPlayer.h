bac8765f95ae58c7be1847db30ad52b21c082f13
emito-k <65490872+emito-k@users.noreply.github.com>
2022-11-07 22:58:22 +0200

    Added comments

--- Source Code/ComputerPlayer.h
+++ Source Code/ComputerPlayer.h
@@ -9,6 +9,9 @@ public:
     * sets the #playerName to @param playerName and sets #playerType to "Computer"
     */
     ComputerPlayer(std::string playerName = "Bobert");
+    /**
+    * This function prompts itself to select a command and pass it to the #country object's executeCommand function
+    */
     void performTurn() override;
 };
 
